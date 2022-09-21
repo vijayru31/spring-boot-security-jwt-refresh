04b8c36f77b3301e9471754f8248e6b7247242bb
vijay <vijayru31@gmail.com>
2022-09-21 12:02:03 +0530

    From feature-branch

--- src/main/java/com/javainuse/springbootsecurity/SpringBootSecurityApplication.java
+++ src/main/java/com/javainuse/springbootsecurity/SpringBootSecurityApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootSecurityApplication {
-
+// from feature-branch 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityApplication.class, args);
 	}
